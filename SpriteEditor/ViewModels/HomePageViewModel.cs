4a54ca03b16305f66a07e088868a947ba69c2d4d
kasaikoun <kasiakou.n@gmail.com>
2021-09-19 13:40:23 +0300

    Add OpenCommand method to load and convert sprite sheets Modify SaveFile method to save file with DialogWindow Remove initialize SpriteSheet by default

--- SpriteEditor/ViewModels/HomePageViewModel.cs
+++ SpriteEditor/ViewModels/HomePageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows;
 using System.Windows.Media.Imaging;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
+using Ookii.Dialogs.Wpf;
 using SpriteDrawer;
 using SpriteEditor.ViewModels.SpriteSheets;
 
@@ -15,22 +17,29 @@ namespace SpriteEditor.ViewModels
 {
     public class HomePageViewModel : MvxViewModel
     {
-        private const string PathToSprite = @"e:\Projects\mk3\src\assets\data\sprites\cyrax.json";
-
         private readonly SpriteSheetConverter _loader = new SpriteSheetConverter();
 
         private object _selectedItem;
         private FrameViewModel _selectedFrame;
         private SpriteSheetViewModel _selectedSpriteSheet;
         private AnimationViewModel _selectedAnimation;
+        private string _lastAssetsPath;
 
         private MvxCommand _saveCommand;
+        private MvxCommand _openCommand;
         private MvxCommand _addAnimationCommand;
         private MvxCommand _addFrameCommand;
 
-        public List<SpriteSheetViewModel> SpriteSheets { get; private set; }
+        public List<SpriteSheetViewModel> SpriteSheets
+        {
+            get => _spriteSheets;
+            private set => SetProperty(ref _spriteSheets, value);
+        }
 
-        public List<FrameViewModel> AvailableFrames => SpriteSheets[0].Animations.SelectMany(p => p.Frames).ToList();
+        public List<FrameViewModel> AvailableFrames => SelectedSpriteSheet?.Animations?.SelectMany(p => p.Frames).ToList();
+
+        public readonly Dictionary<string, BitmapImage> BitmapImageDictionary = new Dictionary<string, BitmapImage>();
+        private List<SpriteSheetViewModel> _spriteSheets;
 
         public object SelectedItem
         {
@@ -62,7 +71,11 @@ namespace SpriteEditor.ViewModels
         public SpriteSheetViewModel SelectedSpriteSheet
         {
             get => _selectedSpriteSheet;
-            set => SetProperty(ref _selectedSpriteSheet, value);
+            set
+            {
+                SetProperty(ref _selectedSpriteSheet, value);
+                RaisePropertyChanged(() => AvailableFrames);
+            }
         }
 
         public AnimationViewModel SelectedAnimation
@@ -77,13 +90,114 @@ namespace SpriteEditor.ViewModels
             set => SetProperty(ref _selectedFrame, value);
         }
 
-        public BitmapImage SpriteBitmap { get; private set; }
-
         public Point InitialPosition { get; set; }
 
         public MvxCommand SaveCommand => _saveCommand ?? (new MvxCommand(SaveFile));
+        public MvxCommand OpenCommand => _openCommand ?? (new MvxCommand(OpenFolder));
         public MvxCommand AddAnimationCommand => _addAnimationCommand ?? (new MvxCommand(AddAnimation));
         public MvxCommand AddFrameCommand => _addFrameCommand ?? (new MvxCommand(AddFrame));
+
+        public void OpenFolder()
+        {
+            var folderDialog = new VistaFolderBrowserDialog();
+            if (!folderDialog.ShowDialog().GetValueOrDefault(false)) return;
+
+            _lastAssetsPath = GetAssetsPath(folderDialog.SelectedPath);
+            if (_lastAssetsPath == null)
+            {
+                MessageBox.Show("This path does not contain assets directory",
+                    "Invalid path",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+
+            var directories = Directory
+                .GetDirectories(folderDialog.SelectedPath, string.Empty, SearchOption.AllDirectories)
+                .Select(p => new DirectoryInfo(p))
+                .ToList();
+            var spriteSheets = new List<SpriteSheetViewModel>();
+            foreach (var directory in directories)
+            {
+                var files = directory.GetFiles("*.json");
+                var createdSpriteSheets = GetSpriteSheets(files);
+                spriteSheets.AddRange(createdSpriteSheets);
+            }
+
+            if (spriteSheets.Count == 0) return;
+
+            foreach (var spriteSheet in spriteSheets)
+            {
+                FillBitmap(spriteSheet);
+            }
+
+            SpriteSheets = spriteSheets;
+            SelectedItem = SpriteSheets[0];
+        }
+
+        private bool FillBitmap(SpriteSheetViewModel spriteSheetViewModel)
+        {
+            if (!BitmapImageDictionary.ContainsKey(spriteSheetViewModel.ImagePath))
+            {
+                var pathToImage = Path.Combine(_lastAssetsPath, spriteSheetViewModel.ImagePath);
+                try
+                {
+                    var bitmap = new BitmapImage(new Uri(pathToImage, UriKind.Absolute));
+                    BitmapImageDictionary[spriteSheetViewModel.ImagePath] = bitmap;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+
+            spriteSheetViewModel.SpriteImage = BitmapImageDictionary[spriteSheetViewModel.ImagePath];
+
+            return true;
+        }
+
+        private string GetAssetsPath(string path)
+        {
+            if (path == null) return null;
+
+            var pathName = Path.GetFileName(path);
+            if (pathName == "assets") return path;
+
+            return GetAssetsPath(Path.GetDirectoryName(path));
+        }
+
+        private List<SpriteSheetViewModel> GetSpriteSheets(FileInfo[] jsonFiles)
+        {
+            var spriteSheets = new List<SpriteSheetViewModel>();
+            foreach (var jsonFile in jsonFiles)
+            {
+                var spriteSheetViewModel = CreateSpriteSheetByPath(jsonFile.FullName);
+                if (spriteSheetViewModel == null) continue;
+                spriteSheets.Add(spriteSheetViewModel);
+            }
+
+            return spriteSheets;
+        }
+
+        private SpriteSheetViewModel CreateSpriteSheetByPath(string jsonPath)
+        {
+            try
+            {
+                var spriteSheet = _loader.LoadSpriteSheetByPath(jsonPath);
+                var spriteSheetViewModel = new SpriteSheetViewModel(spriteSheet)
+                {
+                    PathToFile = jsonPath
+                };
+
+
+                return spriteSheetViewModel;
+            }
+            catch
+            {
+                return null;
+            }
+        }
 
         public void AddAnimation()
         {
@@ -122,21 +236,26 @@ namespace SpriteEditor.ViewModels
 
         public void SaveFile()
         {
-            _loader.SaveSpriteSheetByPath(SpriteSheets[0].Model, PathToSprite);
+            if (SelectedSpriteSheet == null) return;
+
+            if (string.IsNullOrEmpty(SelectedSpriteSheet.PathToFile))
+            {
+                var fileDialog = new VistaSaveFileDialog
+                {
+                    FileName = SelectedSpriteSheet.Name,
+                    DefaultExt = ".json",
+                    Filter = "JavaScript Object Notation (.json)|*.json"
+                };
+                var result = fileDialog.ShowDialog();
+                if (!result.GetValueOrDefault(false)) return;
+            }
+
+            _loader.SaveSpriteSheetByPath(SelectedSpriteSheet.Model, SelectedSpriteSheet.PathToFile);
         }
 
-        public override async Task Initialize ()
+        public override async Task Initialize()
         {
             await base.Initialize();
-
-            var spriteSheet = _loader.LoadSpriteSheetByPath(PathToSprite);
-
-            var spriteSheetViewModel = new SpriteSheetViewModel(spriteSheet);
-            SpriteSheets = new List<SpriteSheetViewModel> { spriteSheetViewModel };
-            SelectedItem = SpriteSheets[0];
-
-            const string pathToImage = @"e:\Projects\mk3\src\assets\graphics\units\cyrax.png";
-            SpriteBitmap = new BitmapImage(new Uri(pathToImage, UriKind.Absolute));
 
             InitialPosition = new Point(50, 50);
         }
--- SpriteEditor/ViewModels/SpriteSheets/SpriteSheetViewModel.cs
+++ SpriteEditor/ViewModels/SpriteSheets/SpriteSheetViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
+using System.Windows.Media.Imaging;
 using MvvmCross.ViewModels;
 using SpriteDrawer;
 
@@ -10,9 +11,38 @@ namespace SpriteEditor.ViewModels.SpriteSheets
 {
     public class SpriteSheetViewModel : MvxNotifyPropertyChanged
     {
+        private BitmapImage _spriteImage;
         public SpriteSheet Model { get; }
 
         public MvxObservableCollection<AnimationViewModel> Animations { get; }
+
+        public string ImagePath
+        {
+            get => Model.Image;
+            set
+            {
+                Model.Image = value;
+                RaisePropertyChanged(() => ImagePath);
+            }
+        }
+
+        public BitmapImage SpriteImage
+        {
+            get => _spriteImage;
+            set => SetProperty(ref _spriteImage, value);
+        }
+
+        public string Name
+        {
+            get => Model.Name;
+            set
+            {
+                Model.Name = value;
+                RaisePropertyChanged(() => Name);
+            }
+        }
+
+        public string PathToFile { get; set; }
 
         public SpriteSheetViewModel(SpriteSheet model)
         {
